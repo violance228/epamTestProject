530c93e4ccb9c0d955797cbbf618565a9b45be9d
violance228 <sviripa1234@gmail.com>
2018-12-20 16:20:38 +0200

    refactor

--- src/main/java/com/violence/util/api/EntityAdapter.java
+++ src/main/java/com/violence/util/api/EntityAdapter.java
@@ -11,9 +11,9 @@ public class EntityAdapter<T> {
     public List<T> getListObjectFromResultSet(T t, ResultSet resultSet) throws SQLException {
         List<T> result = new ArrayList<>();
         if (t instanceof DomainObject) {
-            while (resultSet.next()) {
+            while (resultSet.next())
                 result.add((T) ((DomainObject) t).getObject(resultSet));
-            }
+
             return result;
         } else {
             return null;
@@ -23,7 +23,7 @@ public class EntityAdapter<T> {
     public T getObjectFromResultSet(T t, ResultSet resultSet) throws SQLException {
         if (t instanceof DomainObject) {
             while (resultSet.next())
-                return t = (T) ((DomainObject) t).getObject(resultSet);
+                return (T) ((DomainObject) t).getObject(resultSet);
             return null;
         } else {
             return null;
